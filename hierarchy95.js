418868fccb6e8da086910f28ef590404b2365d91
Sergey Ulyanov <ulyanoow@gmail.com>
2026-08-21 02:36:03 -0700

    Center Stay Strong on mobile

--- hierarchy95.js
+++ hierarchy95.js
@@ -53,7 +53,7 @@
     if (!document.querySelector('link[data-desktop-reality]')) {
       const css = document.createElement('link');
       css.rel = 'stylesheet';
-      css.href = '/desktop-reality.css?v=20260821-4';
+      css.href = '/desktop-reality.css?v=20260821-5';
       css.dataset.desktopReality = '1';
       document.head.appendChild(css);
     }
